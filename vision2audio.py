192dbccbebb5b06506da867fd9c19253ceb9553d
Hector Rodriguez <hector.rodriguez49@upr.edu>
2019-05-04 17:56:38 -0400

    midi gen note correction implemented

--- vision2audio.py
+++ vision2audio.py
@@ -3,7 +3,7 @@ from midiutil import MIDIFile
 from statistics import median
 
 
-m = Image.open("./scaler/logo.png")
+m = Image.open("./scaler/turtle.jpg")
 rgb_im = m.convert('RGB')
 
 w, h = m.size
@@ -16,6 +16,7 @@ channel = 0  # Channel (default 0)
 time = 0  # In beats
 tempo = 90  # In BPM
 volume = 100
+duration = 1
 
 output_arr = list()
 
@@ -45,20 +46,86 @@ for row in range(h):
             if note > 108:
                 note = 108
 
-            duration = 1
-
             output_arr.append(note)
 
-            output.addNote(track, channel, note, time, duration, volume)
-            time += duration
-
             # reset everything
-            iter=0
+            iter = 0
             arrB = []
             arrG = []
             arrR = []
 
-print(output_arr)
+BASE = {
+    'C': 24,
+    'D': 26,
+    'E': 28,
+    'F': 29,
+    'G': 31,
+    'A': 33,
+    'B': 35
+};
+
+SCALES = {
+    'A': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'],
+    'B': ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#', 'B'],
+    'C': ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
+    'D': ['D', 'E', 'F#', 'G', 'A', 'B', 'C#'],
+    'E': ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#'],
+    'F': ['F', 'G', 'A', 'Bb', 'C', 'D', 'E'],
+    'G': ['G', 'A', 'B', 'C', 'D', 'E', 'F#'],
+}
+
+KEYS = {}
+for octave in range(-2, 8):
+    for k in BASE:
+        key = BASE[k] + (octave * 12)
+        KEYS[str(k) + 'b' + str(octave)] = key - 1
+        KEYS[str(k) + str(octave)] = key
+        KEYS[str(k) + '#' + str(octave)] = key + 1
+
+print('original:', output_arr)
+
+corrected_output = list()
+
+for i, val in enumerate(output_arr):
+    for kv in KEYS:
+        if val == KEYS[kv]:
+            if '#' in kv:
+                splitted = kv.split('#')
+
+                if splitted[0] == 'G':
+                    nextChord = 'A'
+                    octave = str(int(splitted[1]) + 1)
+                else:
+                    nextChord = chr(ord(splitted[0]) + 1)
+                    octave = splitted[1]
+
+                corrected = ''.join([nextChord, octave])
+
+                corrected_output.append(KEYS.get(corrected))
+            elif 'b' in kv:
+                splitted = kv.split('b')
+
+                if splitted[0] == 'C':
+                    nextChord = 'B'
+                    octave = str(int(splitted[1]) - 1)
+                else:
+                    nextChord = chr(ord(splitted[0]) - 1)
+                    octave = splitted[1]
+
+                corrected = ''.join([nextChord, octave])
+
+                corrected_output.append(KEYS.get(corrected))
+
+            else:
+                corrected_output.append(val)
+
+            break
+
+print('corrected:', corrected_output)
+
+for note in corrected_output:
+    output.addNote(track, channel, note, time, duration, volume)
+    time += duration
 
 with open("test.mid", "wb") as output_file:
     output.writeFile(output_file)
